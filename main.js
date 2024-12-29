5ac63d8b61b7347c1d76f4a0ae719542a01a478d
Aman R Bhagat <104529303+aamanbhagat@users.noreply.github.com>
2024-12-30 00:54:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Add any global JavaScript functionality here
+    console.log('The Money Focus games are loaded and ready to play!');
+});
+
